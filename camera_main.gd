4d14cfb200bfc379cf4e31aa90ebe6940cbc5de1
sugarvoid <gamedev@kitargo.com>
2023-01-11 07:25:59 -0600

    Make camera smoothly slide to next position

--- camera_main.gd
+++ camera_main.gd
@@ -1,5 +1,9 @@
 extends Camera2D
 
+signal moving_position
+signal done_moving
+
+const PAN_SPEDD: float = 0.75
 
 const SCREEN_SIZE: Vector2 = Vector2( 128, 128)
 var cur_screen = Vector2(0,0)
@@ -20,5 +24,9 @@ func _physics_process(delta: float) -> void:
 func _update_screen(new_screen: Vector2) -> void:
 	print('updating screen')
 	self.cur_screen = new_screen
-	self.global_position = cur_screen * SCREEN_SIZE + SCREEN_SIZE * 0.5
+	var new_pos = cur_screen * SCREEN_SIZE + SCREEN_SIZE * 0.5
+	print('starting tween')
+	$Tween.interpolate_property(self, "global_position", self.global_position, new_pos, PAN_SPEDD, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	$Tween.start()
+	#### self.global_position = cur_screen * SCREEN_SIZE + SCREEN_SIZE * 0.5
 	print(self.global_position)
